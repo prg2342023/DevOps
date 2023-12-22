09388b3db0b42b4f5d714bd2d4b59ad031cae710
prg2342023 <152591800+prg2342023@users.noreply.github.com>
2023-12-22 10:13:21 +0530

    Add files via upload

---
+++ op.py
@@ -0,0 +1,34 @@
+"""
+a=10
+b=5
+c=0
+c=a+b
+print("addition : ",c)
+
+c=a-b
+print("subtraction : ",c)
+
+c=a*b
+print("multiplication : ",c)
+
+c=a%b
+print("modules : ",c)
+
+c=a/b
+print("Division : ",c)
+
+c=a**b
+print("Exponent : ",c)
+
+c=a//b
+print("Floor Division : ",c)
+
+output
+addition :  15
+subtraction :  5
+multiplication :  50
+modules :  0
+Division :  2.0
+Exponent :  100000
+Floor Division :  2
+"""
