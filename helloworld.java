cd21e0cf0dfe3eadca03bb0e41135e78d6497bb9
prg2342023 <152591800+prg2342023@users.noreply.github.com>
2024-01-20 14:06:07 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class HelloWorld { 
 	public static void main(String args[]) 
 	{ 
-		System.out.println("Hello, Prakash Gohil"); 
+		System.out.println("Hello, Prakash Gohil. I'm from India."); 
 	} 
 }
