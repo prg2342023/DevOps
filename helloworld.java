8e3be9566307e20479b592e8dd9559b380d97612
prg2342023 <152591800+prg2342023@users.noreply.github.com>
2024-01-21 11:50:37 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 class HelloWorld { 
 	public static void main(String args[]) 
 	{ 
-		Scanner scanner = new Scanner(System.in);
+		/*Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter your name: ");
 		String userName = scanner.nextLine();
         	System.out.println("Your name is: " + userName);
-		
+		*/
 		System.out.println("Hello,I'm Prakash Gohil. This task performed by Jenkins linked with GitHub Repository.");
 	} 
 }
