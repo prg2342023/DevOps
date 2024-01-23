4f810d6412de72e3f474e8c654196b6713fd401e
prg2342023 <152591800+prg2342023@users.noreply.github.com>
2024-01-23 12:44:04 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 class HelloWorld { 
 	public static void main(String args[]) 
 	{ 
-		System.out.println("Hello, I'm Prakash Gohil.");
+		System.out.println("Hello, I'm Pm Gohil.");
 		System.out.println("This task performed by Jenkins linked with GitHub Repository.");
 	} 
 }
