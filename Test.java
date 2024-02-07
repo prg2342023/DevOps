84c484cbc1f4b4cd5004fae40c3664e90ac03747
prg2342023 <152591800+prg2342023@users.noreply.github.com>
2024-02-07 18:23:21 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ class Test {
 	public static void main(String args[]) 
 	{ 
 		System.out.println("I am Pm Gohil");
-		System.out.println("This task is performed by Jenkins linked with GitHub Repository.");
+		System.out.println("This task is performed by Jenkins linked with GitHub Repository and Slack.");
 	} 
 }
