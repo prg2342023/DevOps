d7ff5d5cba6e66670f252bdbb27bb02c96a03b9e
prg2342023 <152591800+prg2342023@users.noreply.github.com>
2024-02-09 12:21:14 +0530

    Update both.java

--- both.java
+++ both.java
@@ -1,7 +1,7 @@
 class both { 
 	public static void main(String args[]) 
 	{ 
-		System.out.println("I am Pm Gohil. This is both.java");
-		System.out.println("This task is performed by Jenkins linked with GitHub Repository and Slack")
+		System.out.println("I am Pm Gohil. This is both.java"
+		System.out.println("This task is performed by Jenkins linked with GitHub Repository and Slack");
 	} 
 }
